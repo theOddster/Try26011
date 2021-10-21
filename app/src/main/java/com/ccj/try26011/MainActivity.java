16a1764784972ae074dbd319e99645cd2dd1c7f2
Jeric <jeric292@gmail.com>
2021-10-21 18:30:34 +0800

    Second Update

--- app/src/main/java/com/ccj/try26011/MainActivity.java
+++ app/src/main/java/com/ccj/try26011/MainActivity.java
@@ -2,13 +2,30 @@ package com.ccj.try26011;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+    }
+
+    private void openLogin(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ccj/try26011/instruction.java
@@ -0,0 +1,13 @@
+package com.ccj.try26011;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class instruction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
