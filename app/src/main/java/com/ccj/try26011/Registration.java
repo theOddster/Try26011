7b12a7a627f7145dc9bc1cee34022c287e30ae7d
Chloe Garcia <cavgarcia56@gmail.com>
2021-10-22 17:24:52 +0800

    a

--- app/src/main/java/com/ccj/try26011/Registration.java
+++ app/src/main/java/com/ccj/try26011/Registration.java
@@ -13,7 +13,7 @@ public class Registration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
         signup = (Button) findViewById(R.id.signup);
         signup.setOnClickListener(new View.OnClickListener(){
