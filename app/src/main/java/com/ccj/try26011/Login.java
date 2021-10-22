0462ff23c44c3b45087ade8715f2f7866534e76b
Chloe Garcia <cavgarcia56@gmail.com>
2021-10-22 17:09:10 +0800

    a

--- app/src/main/java/com/ccj/try26011/Login.java
+++ app/src/main/java/com/ccj/try26011/Login.java
@@ -2,7 +2,9 @@ package com.ccj.try26011;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,5 +18,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        bRegister=(Button) findViewById(R.id.signup) ;
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
