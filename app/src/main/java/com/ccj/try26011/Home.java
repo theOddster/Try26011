4217a6e78bfeee5a5f9109443ccf6945bb88ccfb
Jeric <jeric292@gmail.com>
2021-11-10 17:00:59 +0800

    Jeric's Update

--- app/src/main/java/com/ccj/try26011/Home.java
+++ app/src/main/java/com/ccj/try26011/Home.java
@@ -2,13 +2,29 @@ package com.ccj.try26011;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button bHomeProfile;
+    DBUser db = new DBUser(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bHomeProfile = (Button) findViewById(R.id.bhomeProfile);
+        bHomeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),UserProfile.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
